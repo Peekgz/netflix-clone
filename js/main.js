ce6bdbd0dd6a0517325d15ef763f528d1a3c719a
Mariami Zardiashvili <zardiashviligiorgi17@gmail.com>
2020-06-29 23:31:52 +0400

    netflix clone

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+//Select tab content item
+function selectItem(e) {
+  removeBorder();
+  removeBShow();
+  // Add border to current tab
+  this.classlist.add("tab-border");
+  //Grab content item from DOM
+  const tabContentItem = document.querySelector("#${this.id}-content");
+  // Add show class
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
+}
+function removeBShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
+}
+
+//listen for tab click
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
